629c645f2ae9ebaf652bbb0b1de4da61be377c8a
ktsolkas <ktsolkas0@gmail.com>
2022-05-31 02:34:12 +0300

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,56 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom/client";
+
+import * as esbuild from "esbuild-wasm";
+
+const App = () => {
+  const [inputText, setInputText] = useState("");
+  const [outputText, setOutputText] = useState("");
+  let esbuildInitialized = false;
+
+  const startService = async () => {
+    await esbuild.initialize({
+      worker: true,
+      wasmURL: "/esbuild.wasm",
+    });
+    esbuildInitialized = true;
+  };
+  useEffect(() => {
+    startService();
+    //esbuild.build({});
+  }, []);
+
+  const onInputTextChange = (e: React.ChangeEvent<HTMLTextAreaElement>) =>
+    setInputText(e.target.value);
+  const onClick = () => {
+    if (!esbuildInitialized) {
+      return;
+    }
+    console.log(3);
+  };
+
+  return (
+    <div>
+      <textarea
+        placeholder="test"
+        value={inputText}
+        onChange={onInputTextChange}
+      />
+      <div>
+        <button onClick={onClick}>Submit</button>
+      </div>
+      <pre>{outputText}</pre>
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  document.getElementById("root") as HTMLElement
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+root.render(<App />);
